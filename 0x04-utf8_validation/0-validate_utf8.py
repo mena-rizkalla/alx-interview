6e7d11ecce3922271591f2ee58936d4a2a12154e
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-08-04 11:40:37 +0300

    validate utf8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,30 @@
+def validUtf8(self, data: List[int]) -> bool:
+        
+        x = 128
+        y = 64
+        n = 0
+
+        if len(data) == 1 and (data[0] & x) == 0:
+            return True
+
+        if len(data) == 1 and (data[0] & x) != 0:
+            return False
+        
+        for i in range(len(data)):
+            if n == 0:
+                if (data[i] & x) == 0:
+                    continue
+                
+                while((data[i] & x) != 0):
+                    data[i] = data[i] << 1
+                    n = n + 1
+                if n == 1 or n >= 5:
+                    return False
+                n = n-1
+            else:
+                if((data[i] & x) == 0 or (data[i] & y) != 0):
+                    return False
+                n = n - 1;
+
+        return n == 0
+
